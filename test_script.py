e71da68ab76f871f7372ccabb0fe97a4646e7b5f
Robin Singh <robin.singh.1991@gmail.com>
2016-01-09 01:39:28 -0500

    fixes

--- test_script.py
+++ test_script.py
@@ -41,7 +41,7 @@ while ssdb.qsize("ig-queue"):
     f.close()
 
     try:
-        bashCommand = "cat links.txt | parallel --gnu -j20 wget -q -E"
+        bashCommand = "cat links.txt | parallel --gnu -j20 wget -E"
         output = subprocess.check_output(['bash','-c', bashCommand])
     except Exception as e:
         print e
